0a5ac43ba400fd0d926e4040517724383b508311
unknown-user <unknown-user@192.168.0.101>
2015-10-28 11:23:50 -0200

    Nova classe MEnsagens

---
+++ SeverinoApp/Moldel/Mensagem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeverinoApp
+{
+	public class Mensagem
+	{
+		public int ID { get; set; }
+		public int NumeroChamado { get; set; }
+		public int IDSender { get; set; }
+		public int IDUsuario { get; set; }
+		public int IDProfissional { get; set; }
+		public string Texto { get; set; }
+		public byte[] Imagem { get; set; }
+		public int UsuarioVisualizou { get; set; }
+		public int ProfissionalVisualizou { get; set; }
+
+		public DateTime DtCadastro{ get; set; }
+		public List<Mensagem> Mensagens{ get; set;}
+	}
+
+	public class MensagemConsulta : Mensagem
+	{
+		public string UsuarioNome { get; set; }
+		public string ProfissionalNome { get; set; }
+	}
+}
+
